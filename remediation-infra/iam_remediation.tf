6515abf64c368176c86873355ce0b639d541f056
Joe Buhr <joebuhr@google.com>
2026-07-02 20:56:46 +0000

    Enable rem-custom-0007 runner for S3 malware quarantine
    - Add rem-custom-0007 with S3 permissions to iam_remediation.tf.
    - Enable rem-custom-0007 in outpost-lite-values.tpl.yaml.
    
    TAG=agy
    CONV=80898c94-da45-4aaa-90ab-5ea8cfbd3693

--- remediation-infra/iam_remediation.tf
+++ remediation-infra/iam_remediation.tf
@@ -27,6 +27,15 @@ module "remediation_roles" {
       "iam:TagUser",
       "iam:UntagUser",
       "iam:UpdateAccessKey"
+    ],
+    "rem-custom-0007" : [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion",
+      "s3:PutObject",
+      "s3:ListBucket",
+      "s3:ListBucketVersions"
     ]
   }
 }
